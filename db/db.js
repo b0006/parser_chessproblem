1cd558e81d0894157141e8fba2bd4e441fc09251
Alexandr Kasikov <qsefthuken@gmail.com>
2018-08-15 01:25:54 +0700

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,7 +1,7 @@
 let mysql = require('mysql');
 
 let connection = mysql.createConnection({
-    host: "mastersv.ru",
+    host: "localhost",
     user: "64dbu",
     password: "Unhj86*2",
     port: 3306,
